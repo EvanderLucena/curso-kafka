ea325d2c228415b693ba6368282217fffd5d3c92
Evander Lucena <evander.willian@gmail.com>
2024-02-14 11:17:36 -0300

    custumizando a serialização

--- src/main/java/br/com/lucena/KafkaService.java
+++ src/main/java/br/com/lucena/KafkaService.java
@@ -7,17 +7,28 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.io.Closeable;
 import java.time.Duration;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
-public class KafkaService implements Closeable {
-    private final KafkaConsumer<String, String> consumer;
+public class KafkaService<T> implements Closeable {
+    private final KafkaConsumer<String, T> consumer;
     private final ConsumerFunction parse;
 
-    public KafkaService(String groupID, String topic, ConsumerFunction parse) {
-        this.parse = parse;
-        this.consumer = new KafkaConsumer<>(properties(groupID));
+    public KafkaService(String groupID, String topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
+        this(parse, groupID, type, properties);
         consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    public KafkaService(String groupID, Pattern topic, ConsumerFunction parse,  Class<T> type, Map<String, String> properties) {
+        this(parse, groupID, type, properties);
+        consumer.subscribe(topic);
+    }
+
+    private KafkaService(ConsumerFunction parse, String groupID, Class<T> type, Map<String, String> properties) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<>(getProperties(type, groupID, properties));
     }
 
     public void run() {
@@ -32,14 +43,17 @@ public class KafkaService implements Closeable {
         }
     }
 
-    private static Properties properties(String groupID) {
+    private Properties getProperties(Class<T> type, String groupID, Map<String, String> overrideProperties) {
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID);
         properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
         properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        properties.setProperty(GsonDeserializer.TYPE_CONFIG, type.getName());
+        properties.putAll(overrideProperties);
+
         return properties;
 
     }
